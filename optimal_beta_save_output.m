f7d1e82a5e2ba07f584d25d774afdd8066a1ad64
Zofia Stanley <zstanley@Zofias-MacBook-Pro-2.local>
2021-06-01 22:24:13 -0600

    beta for both x and y

--- optimal_beta_save_output.m
+++ optimal_beta_save_output.m
@@ -13,7 +13,8 @@ start = 1001;           % first assimilation cycle considered in RMSE computatio
 
 %% Run Experiments
 
-% 1. All Y, No X
+%{
+%% 1. All Y, No X
 fprintf('All Y, No X.\n')
 dtObs = 0.005;         % Time between assimilation cycles
 Frac_Obs_Y = 1;        % Fraction of Y variables that are observed
@@ -63,7 +64,7 @@ end
 save('optimal_beta.mat', 'b_bw', 'RMSE_Y_YnoX_MVBW', 'RMSE_X_YnoX_MVBW', '-append')
 
 
-% 2. All X, No Y
+%% 2. All X, No Y
 fprintf('\nAll X, No Y.\n')
 dtObs = 0.05;          % Time between assimilation cycles
 Frac_Obs_Y = 0;        % Fraction of Y variables that are observed
@@ -111,4 +112,56 @@ for ii = 1:length(b_bw)
     RMSE_X_XnoY_MVBW(ii) = mean(reshape(RMSE_X(:, start:end), 1, []));
 end
 save('optimal_beta.mat', 'RMSE_Y_XnoY_MVBW', 'RMSE_X_XnoY_MVBW', '-append')
+
+%}
+%% 3. Both X and Y
+fprintf('\nBoth X and Y.\n')
+dtObs = 0.005;          % Time between assimilation cycles
+Frac_Obs_Y = 0.75;      % Fraction of Y variables that are observed
+Frac_Obs_X = 0.75;      % Fraction of X variables that are observed
+sigma2Y = 0.01;         % Y obs error variance
+sigma2X = 0.57;         % X obs error variance
+rYY = 15;
+rXX = 40;
+
+% Beta max for each function
+bmax_gc = gaspari_cohn_beta_max(struct('rYY', rYY, 'rXX', rXX));
+b_gc = [0.1:0.1:bmax_gc, bmax_gc];
+bmax_bw = bolin_wallin_beta_max(struct('rYY', rYY, 'rXX', rXX));
+b_bw = [0.1:0.1:bmax_bw, bmax_bw];
+
+% Initialize arrays
+RMSE_Y_BothXY_MVGC = zeros(1, length(b_gc));
+RMSE_X_BothXY_MVGC = zeros(1, length(b_gc));
+RMSE_Y_BothXY_MVBW = zeros(1, length(b_bw));
+RMSE_X_BothXY_MVBW = zeros(1, length(b_bw));
+
+% 3A. Gaspari-Cohn
+fprintf('\nGaspari-Cohn\n')
+loc_fun_name = 'gaspari_cohn' ; % localization function name
+loc_params = struct('rYY', rYY, 'rXX', rXX); % localization parameters
+for ii = 1:length(b_gc)
+    loc_params.beta = b_gc(ii);
+    [RMSE_Y, RMSE_X] = L96_EnKF_Multitrial(params, Nt, dtObs, sigma2Y, sigma2X,... 
+                                Frac_Obs_Y, Frac_Obs_X, Ne, x_position, rInf, Adapt_Inf, ...
+                                loc_fun_name, loc_params, True_Fcast_Err, Ntrial);
+    RMSE_Y_BothXY_MVGC(ii) = mean(reshape(RMSE_Y(:, start:end), 1, []));
+    RMSE_X_BothXY_MVGC(ii) = mean(reshape(RMSE_X(:, start:end), 1, []));
+end
+save('optimal_beta.mat', 'RMSE_Y_BothXY_MVGC', 'RMSE_X_BothXY_MVGC', '-append')
+
+
+% 3B. Bolin-Wallin
+fprintf('\nBolin-Wallin\n')
+loc_fun_name = 'bolin_wallin' ;
+loc_params = struct('rYY', rYY, 'rXX', rXX); % localization parameters
+for ii = 1:length(b_bw)
+    loc_params.beta = b_bw(ii);
+    [RMSE_Y, RMSE_X] = L96_EnKF_Multitrial(params, Nt, dtObs, sigma2Y, sigma2X,... 
+                                Frac_Obs_Y, Frac_Obs_X, Ne, x_position, rInf, Adapt_Inf, ...
+                                loc_fun_name, loc_params, True_Fcast_Err, Ntrial);
+    RMSE_Y_BothXY_MVBW(ii) = mean(reshape(RMSE_Y(:, start:end), 1, []));
+    RMSE_X_BothXY_MVBW(ii) = mean(reshape(RMSE_X(:, start:end), 1, []));
+end
+save('optimal_beta.mat', 'RMSE_Y_BothXY_MVBW', 'RMSE_X_BothXY_MVBW', '-append')
 
